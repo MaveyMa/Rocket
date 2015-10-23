47e9ae615feb87ec765db3f11da0e59abbd04a61
NancyGomez <nancyhgomez@student.hartnell.edu>
2015-10-23 13:14:05 -0700

    Create Rosas_rocketC.cpp

---
+++ Rosas_rocketC.cpp
@@ -0,0 +1,22 @@
+// Nancy Gomez
+// Mavey Ma
+// Israel --
+// Nicholas --
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+
+
+
+
+
+	return 0;
+}
