1475a41168a158bfb006929c60f916b7a5a86d72
Nancy Gomez <nancyhgomez@student.hartnell.edu>
2015-10-23 13:11:49 -0700

    Added the data file and finished the skeleton of the file rocketD.cpp

--- Gomez_rocketD.cpp
+++ Gomez_rocketD.cpp
@@ -2,3 +2,21 @@
 // Mavey Ma
 // Israel --
 // Nicholas --
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+
+
+
+
+
+	return 0;
+}
